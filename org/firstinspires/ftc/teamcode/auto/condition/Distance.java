d13c300fe562e98f0c04a1e127c01c937801389c
Chris <chris.muller@outlook.com>
2017-12-14 15:21:05 -0700

    whack

--- org/firstinspires/ftc/teamcode/auto/condition/Distance.java
+++ org/firstinspires/ftc/teamcode/auto/condition/Distance.java
@@ -1,22 +1,37 @@
 package org.firstinspires.ftc.teamcode.auto.condition;
 
+import org.firstinspires.ftc.teamcode.module.*;
+
 public class Distance extends Condition {
-    private double initialEncoder;
-    private double distance;
+    private double goalDistance;
+    private double avgDistance;
     private Motor[] motors;
+    private double[] initialEncoders;
     private double[] distanceDiffs;
 
     public Distance(Motor[] m, double dis) {
-        distance = dis;
+        goalDistance = dis;
         motors = new Motor[m.length];
+        initialEncoders = new Double[m.length];
         distanceDiffs = new Double[m.length];
+        for (int i = 0; i < motors.length; i++) {
+            motors[i] = m[i];
+        }
     }
 
     public void init() {
-        initialEncoder = (double)motor.getEncoder();
+        for (int i = 0; i < motors.length; i++) {
+            initialEncoders[i] = motors[i].getEncoder();
+        }
     }
 
     public boolean loop() {
-        if
+        avgDistance = 0;
+        for (int i = 0; i < motors.length; i++) {
+            distanceDiffs[i] = motors[i].getEncoder() - initialEncoders[i];
+            avgDistance += distanceDiffs[i];
+        }
+        avgDistance = (avgDistance / motors.length);
+        return (avgDistance >= goalDistance);
     }
 }
