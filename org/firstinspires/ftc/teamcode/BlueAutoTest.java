aec373ca039c80658f4565dd2aa5208314b54097
Brooks J Rady <b.j.rady@gmail.com>
2018-01-19 15:49:41 -0700

    Add a blue OpMode

---
+++ org/firstinspires/ftc/teamcode/BlueAutoTest.java
@@ -0,0 +1,52 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import org.firstinspires.ftc.teamcode.module.*;
+import org.firstinspires.ftc.teamcode.auto.*;
+import org.firstinspires.ftc.teamcode.auto.condition.*;
+import java.util.ArrayList;
+
+@Autonomous(name="BlueAutoTest", group="Opmode")
+
+public class BlueAutoTest extends OpMode{
+
+    private OmniDrive chassis;
+    //private LinearArm arm;
+    private ArrayList<Instruct> script;
+
+    private long startTime;
+
+    private long deltaTime;
+
+    @Override
+    public void init() {
+        script = new ArrayList<Instruct>();
+        chassis = new OmniDrive(hardwareMap);
+        //arm = new LinearArm(hardwareMap);
+
+        script.add(new MoveEye(chassis, MoveEye.Direction.DOWN, new EyeAtPosition(chassis)));
+        script.add(new Wait(chassis, new Time(10)));
+        script.add(new Jewel(chassis, Jewel.Color.RED, new Time(3)));
+        script.add(new Wait(chassis, new Time(20)));
+        script.add(new MoveEye(chassis, MoveEye.Direction.UP, new EyeAtPosition(chassis)));
+        //script.add(new Drive(chassis, OmniDrive.Direction.C, new Time(30)));
+        //script.add(new Wait(chassis, new Time())
+        //script.add(new Drive(chassis, OmniDrive.Direction.F, new Distance(chassis, 1806.0)));
+        //script.add(new Wait(chassis, new Time(100)));
+        //script.add(new Drive(chassis, OmniDrive.Direction.B, new Distance(chassis, 1806.0)));
+        //script.add(new Wait(chassis, new Time(100)));
+        startTime = System.currentTimeMillis();
+    }
+
+    @Override
+    public void loop() {
+        if(script.size() != 0) {
+            if(script.get(0).tick()) {
+                script.remove(0);
+            }
+        }
+        telemetry.addData("Is Blue?: ", chassis.isBlue());
+        //arm.tick();
+    }
+}
